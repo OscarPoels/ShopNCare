68cc83d3f9aceb5dfbe3b8476bd363edc42fea50
zexalo <zexal99@icloud.com>
2021-06-14 00:08:28 +0200

    change class to functionnal component and navigate

--- frontend/screens/index.js
+++ frontend/screens/index.js
@@ -4,10 +4,12 @@ import Restaurants from './Restaurants';
 import Login from './Login';
 import LoginOrSignIn from './LoginOrSignIn';
 import ResetPassword from './ResetPassword';
-import ResetPasswordMail from './ResetPasswordEmail';
+import ResetPasswordEmail from './ResetPasswordEmail';
 import SignUp from './SignUp';
 import Welcome from './Welcome';
 import WelcomeInformations from './WelcomeInformations';
 
 
-export { Home, OrderDelivery, Restaurants,Login, LoginOrSignIn, ResetPassword, ResetPasswordMail, SignUp, Welcome, WelcomeInformations };
+export { Home, OrderDelivery, 
+    Restaurants,Login, LoginOrSignIn, ResetPassword, 
+    ResetPasswordEmail, SignUp, Welcome, WelcomeInformations };
